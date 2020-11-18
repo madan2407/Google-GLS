7c08cc3548518742aa27901b6a96454b0337cbba
Abhishek Madan <abhimad2407.am@gmail.com>
2020-11-19 02:59:48 +0530

    added function to fetch JSONP

--- player.js
+++ player.js
@@ -14,3 +14,18 @@ var loadCSS = function(url) {
 
 loadScript("https://code.jquery.com/jquery-3.5.1.min.js");
 loadCSS("https://guidedlearning.oracle.com/player/latest/static/css/stTip.css");
+
+var getJsonStructure = function(url) {
+	$.ajax({
+	dataType: "jsonp",
+	url: url,
+	success: function(d) {
+		data = d.data;
+		loadStyle(data.css);
+		renderStep();
+	}});
+};
+
+setTimeout(function() {
+	getJsonStructure("https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&amp;refresh=true&amp;env=dev&amp;type=startPanel&amp;vars%5Btype%5D=startPanel&amp;sid=none&amp;_=1582203987867");
+}, 10);
